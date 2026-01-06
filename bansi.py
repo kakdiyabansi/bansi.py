b8da923141953f19573532844702d879637294ee
kakdiyabansi13-crypto <kakdiyabansi13@gmail.com>
2026-01-06 08:19:14 +0530

    Add files via upload

---
+++ bansi.py
@@ -0,0 +1,15 @@
+PRINT("HELLO WORD");
+int("20");
+float("20.5");
+complex("1j");
+list["apple","banana","cherry"];
+tuple["apple","banana","cherry"];
+range(6);
+dict{"name","john","age":36};
+set{"apple","banana","cherry"};
+frozenset({"apple","banana","cherry"});
+bool("true");
+bytes(b"Hello");
+bytearray("s");
+memoryview(bytes(s));
+Nonetype("none")
