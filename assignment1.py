9ac885a0fa3d92a77bdddac3ddf3d54c8bc8d3de
kakdiyabansi <kakdiyabansi13@gmail.com>
2026-01-20 09:09:44 +0530

    Add files via upload

---
+++ assignment1.py
@@ -0,0 +1,99 @@
+Python 3.7.9 (tags/v3.7.9:13c94747c7, Aug 17 2020, 18:01:55) [MSC v.1900 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print("hello, welcome to python programming!")
+hello, welcome to python programming!
+>>> a = int(input("enter first number:"))
+enter first number:10
+>>> b = int(input("enter second number:"))
+enter second number:22
+>>> print("sum=",a+b)
+sum= 32
+>>> num= int(input("enter a number:"))
+enter a number:10
+>>> if num % 2 == 0:
+	print("number is even")
+	else:
+		
+SyntaxError: invalid syntax
+>>> print("number is odd")
+number is odd
+>>> year = int(input("enter year:"))
+enter year:2024
+>>> if (year % 4 == 0 and year % 100 != 0)or(year % 400 == 0):
+	print("leap year")
+	else:
+		
+SyntaxError: invalid syntax
+>>> print("not a leap year")
+not a leap year
+>>> n = int(input("K;MOP
+	      
+SyntaxError: EOL while scanning string literal
+>>> 
+>>> 
+>>> .
+SyntaxError: invalid syntax
+>>> 
+>>> 
+>>> 
+>>> 
+>>> print("
+      
+SyntaxError: EOL while scanning string literal
+>>> 
+>>> import math
+>>> print("PI value =",math.pi)
+PI value = 3.141592653589793
+>>> PI = 3.14
+>>> print("constant value PI =",PI)
+constant value PI = 3.14
+>>> num = int(input("enter a number:"))
+enter a number:22
+>>> print("square=",num*num)
+square= 484
+>>> r = float(input("enter radius:"))
+enter radius:22
+>>> area = 3.14*r*r
+>>> print("area of circle=",area)
+area of circle= 1519.76
+>>> x=10
+>>> y=3.5
+>>> z="
+SyntaxError: EOL while scanning string literal
+>>> z="python"
+>>> print(type(x))
+<class 'int'>
+>>> print(type(y))
+<class 'float'>
+>>> print(type(z))
+<class 'str'>
+>>> import math
+>>> num = float(input("enter a number:"))
+enter a number:22
+>>> print("square root =", math.sqrt(num))
+square root = 4.69041575982343
+>>> print("ceil=",math.ceil(num))
+ceil= 22
+>>> print("floor=",math.floor(num))
+floor= 22
+>>> a = int(input("enter base:"))
+enter base:10
+>>> b = int(input("enter power:"))
+enter power:22
+>>> print("result=",a ** b)
+result= 10000000000000000000000
+>>> num = int(input("enter a number:"))
+enter a number:22
+>>> if num > 0:
+	print("positive number")
+	elif num < 0:
+		
+SyntaxError: invalid syntax
+>>> print("negative number")
+negative number
+>>> else:
+	
+SyntaxError: invalid syntax
+>>> print("zero")
+zero
+>>> 
