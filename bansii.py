452a39e849e35aefe7b14e032b0970e7ba958a41
kakdiyabansi <kakdiyabansi13@gmail.com>
2026-01-27 08:22:00 +0530

    Add files via upload

---
+++ bansii.py
@@ -0,0 +1,66 @@
+Python 3.7.9 (tags/v3.7.9:13c94747c7, Aug 17 2020, 18:01:55) [MSC v.1900 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> #arithmetic operator:
+>>> a=5
+>>> b=10
+>>> print (a+b)
+15
+>>> print (a-b)
+-5
+>>> print (a*b)
+50
+>>> #assignment operator:
+>>> x=10
+>>> print (x)
+10
+>>> #unary operator:
+>>> a=5
+>>> print (-a)
+-5
+>>> #relational operator:
+>>> p=10
+>>> q=5
+>>> print (p>q)
+True
+>>> print (p==q)
+False
+>>> #logical operator:
+>>> x = True
+>>> y = False
+>>> print (x and y)
+False
+>>> print (x or y)
+True
+>>> print (x not y)
+SyntaxError: invalid syntax
+>>> print (not x)
+False
+>>> # boolear operator:
+>>> is pass = True
+SyntaxError: invalid syntax
+>>> Is pass = True
+SyntaxError: invalid syntax
+>>> is_pass = True
+>>> print(is_pass)
+True
+>>> # bitwise operator:
+>>> a=5
+>>> b=3
+>>> print (a&b)
+1
+>>> print (a/b)
+1.6666666666666667
+>>> #membership operator:
+>>> numbers = [1,2,3,4]
+>>> print (2 in numbers)
+True
+>>> print (5 not in numbers)
+True
+>>> # identiyty operator:
+>>> x=10
+>>> y=10
+>>> print (x is y)
+True
+>>> print (x is not y)
+False
+>>> 
