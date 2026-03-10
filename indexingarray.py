561f1f8145c4378d27d155387b7dfae264be94a2
kakdiyabansi <kakdiyabansi13@gmail.com>
2026-03-10 08:17:34 +0530

    Add files via upload

---
+++ indexingarray.py
@@ -0,0 +1,24 @@
+"""#positive indexing
+from array import array
+arr=array('i',[10,20,30,40,50])
+print(arr[0])#first element
+print(arr[2])#thired element
+print(arr[4])#fifth element"""
+
+"""#negative indexing
+from array import array
+arr=array('i',[10,20,30,40,50])
+print(arr[-1])#last element
+print(arr[-2])#second last element
+print(arr[-5])#first element"""
+
+"""#diagram
+from array import array
+ARRAY:[10,20,30,40,50]
+Index: 0 1 2 3 4
+Negative: -5 -4 -3 -2 -1"""
+
+#modifying using index
+from array import array
+arr=array('i',[10,20,30,40,50])
+print(arr)
