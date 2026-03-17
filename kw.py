592be72eb20d833ec127a7cb0de13a2c0eef8376
kakdiyabansi <kakdiyabansi13@gmail.com>
2026-03-17 08:29:25 +0530

    Add files via upload

---
+++ kw.py
@@ -0,0 +1,11 @@
+"""def student_info(name,age,city):
+ print("name=",name)
+ print("age=",age)
+ print("city=",city)
+ student_info(age=18,city="rajkot",name="ravi")"""
+
+def display(a,b,c):
+  print("a=",a)
+  print("b=",b)
+  print("c=",c)
+display(1,c=3,b=2)
